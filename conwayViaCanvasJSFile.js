0c9edaa10b30c3567b9ecb48879b535966673119
Eric Oh <77398304+oEriic@users.noreply.github.com>
2021-04-20 10:54:45 -0400

    basically got it but i broke the blinker and boat is wonky😴😴

--- conwayViaCanvasJSFile.js
+++ conwayViaCanvasJSFile.js
@@ -1,40 +1,40 @@
 var grid;
-var rows = 40;
-var cols = 80;
+var copyGrid;
+var row = 40;
+var col = 80;
 
-var enableBoard = false;
-var timer;
+var disableBoard = false;
 var playID;
 var stopID;
 
 function startUp(){
     //create grid
-    grid = new Array(rows).fill(0).map(() => new Array(col).fill(0));
-
+    grid = new Array(row).fill(null).map(() => new Array(col).fill(null));
+    copyGrid = grid.map(elem => elem);
     // create table
     render();
     // ensure grid is clear
-    resetGrid();
+    reset();
 
     //buttons
 
 
 }
 
-// add cells to grid and checks if alive or dead then load onto webpage
-// Credit: https://www.youtube.com/watch?v=deXzu0Eregs
+// add cells to grid and sets if alive or dead then load onto webpage
+// Credit: https://codepen.io/RBSpatz/pen/rLyNLb
 function render(){
     var gridContainer = document.getElementById('gridContainer');
 
-    var table = document.createElement("table");
+    var table = document.createElement('table');
 
-    for (var i = 0; i < rows; i++) {
-		var tr = document.createElement("tr");
-        for (var j = 0; j < cols; j++) {//
-        	var cell = document.createElement("td");
-        	cell.setAttribute("id", i + "_" + j);
-        	cell.setAttribute("class", "dead");
-        	cell.onclick = patternHandler;
+    for (var i = 0; i < row; i++) {
+		var tr = document.createElement('tr');
+        for (var j = 0; j < col; j++) {//
+        	var cell = document.createElement('td');
+        	cell.setAttribute('id', i + '_' + j);
+        	cell.setAttribute('class', 'dead');
+        	cell.onclick = patternBrush;
         	tr.appendChild(cell);
         }
         table.appendChild(tr);
@@ -42,68 +42,148 @@ function render(){
     gridContainer.appendChild(table);
 }
 
+// resets / wipes grid
+function reset(){
+    for(let i = 0; i < row; i++){
+        for(let j = 0; j < col; j++){
+            let cell = document.getElementById(i + '_' + j).setAttribute('class', 'dead');
+        }
+    }
+
+    for(let i = 0; i < row; i++){
+        grid[i].fill(0);
+        copyGrid[i].fill(0);
+    }
+}
+
+// getter for preset patterns
+function getPattern(){
+    // lock buttons while game runs
+    if(disableBoard){
+        return;
+    }
+
+    //pattern data from drop down
+    var pat = document.getElementById('pattern');
+    return parseInt(pat.value); // parse value as int 
+}
+
+// preset pattern setter for user mouse click
+function patternBrush(){
+    //lock screen while game running
+    if(disableBoard){
+        return;
+    }
+
+    // get cords for clicked cell
+    var rowColumn = this.id.split('_');
+    var nRow = parseInt(rowColumn[0]);
+    var nCol = parseInt(rowColumn[1]);
+    var pattern  = getPattern()
+    // set pattern brush
+    switch(pattern) {
+        case 2:
+            // Still life: Block
+            fillCell(grid, nRow, nCol); //top right
+            fillCell(grid, nRow, nCol-1); //top left
+            fillCell(grid, nRow-1, nCol); //bottom right
+            fillCell(grid, nRow-1, nCol-1); // bottom left
+            break;
+        case 3:
+            //  Still life: Boat
+            fillCell(grid, nRow, nCol);// top right
+            fillCell(grid, nRow-1, nCol); // top left
+            fillCell(grid, nRow-1, nCol-1); // bottom left
+            fillCell(grid, nRow-2, nCol-1); // bottom center with space in middle
+            fillCell(grid, nRow-1, nCol+1); // right fin
+            break;
+        case 4:
+            // Osci: Blinker
+            fillCell(grid, nRow, nCol); // center
+            fillCell(grid, nRow, nCol-1); // left
+            fillCell(grid, nRow, nCol+1); // right
+            break;
+        case 5:
+            // Osci: Beacon 
+            //top
+            fillCell(grid, nRow, nCol);//tip of top
+            fillCell(grid, nRow, nCol+1);//right tip
+            fillCell(grid, nRow-1, nCol);//under of tip
+            //bot
+            fillCell(grid, nRow-3, nCol+3);//tip of under
+            fillCell(grid, nRow-3, nCol+2);//left of tip
+            fillCell(grid, nRow-2, nCol+3);//top of tip
+            break;
+        case 6:
+            // Osci: Toad
+            fillCell(grid, nRow, nCol); // base
+            fillCell(grid, nRow, nCol+1);// one right
+            fillCell(grid, nRow, nCol+2); // two right
+
+            fillCell(grid, nRow-1, nCol-1); //under and left of base
+            fillCell(grid, nRow-1, nCol); // under base
+            fillCell(grid, nRow-1, nCol+1);// under of right of base
+            break;    
+        default:
+            //single dot as default (1)
+            fillCell(grid,nRow,nCol);
+      } 
+}
 //count neighbors around the cell
 function countNeighbors(x,y){
     var numNeighbors = 0;
     //checks cells within a 3x3 radius
-    for(let i = -1; i < 2; i++){
-        for(let j = -1; j<2; j++){
-            if(i=== 0 && j === 0){ // excludes self cell
-                continue;
-            }
-            var x_pos = x + i;
-            var y_pos = y + j;
-            // check if cell is within grid boundaries
-            if(x_pos >= 0 && y_pos >= 0 && x_pos < col && y_pos < row){
-                const currentNeighbor = grid[x_pos][y_pos];
-                numNeighbors += currentNeighbor;
-            }
-        }
-    }
+    if (x-1 >= 0) {
+		if (grid[x-1][y] == 1) numNeighbors++;
+	}
+	if (x-1 >= 0 && y-1 >= 0) {
+		if (grid[x-1][y-1] == 1) numNeighbors++;
+	}
+	if (x-1 >= 0 && y+1 < col) {
+		if (grid[x-1][y+1] == 1) numNeighbors++;
+	}
+	if (y-1 >= 0) {
+		if (grid[x][y-1] == 1) numNeighbors++;
+	}
+	if (y+1 < col) {
+		if (grid[x][y+1] == 1) numNeighbors++;
+	}
+	if (x+1 < row) {
+		if (grid[x+1][y] == 1) numNeighbors++;
+	}
+	if (x+1 < row && y-1 >= 0) {
+		if (grid[x+1][y-1] == 1) numNeighbors++;
+	}
+	if (x+1 < row && y+1 < col) {
+		if (grid[x+1][y+1] == 1) numNeighbors++;
+	}
     return numNeighbors;
 }
 
 
 
 function displayNextGen() {
-    // copy grid to apply rules so we dont check the cells that we are changing and run into checking loops
-    var copyGrid = grid.map(elem => elem);
-
     // traverse array applying rules to each cell
-    for(var x_pos = 0; x_pos < grid.length; x_pos++){
-        for(var y_pos = 0; y_pos < grid[x_pos].length; y_pos++){
+    for(var x_pos = 0; x_pos < row; x_pos++){
+        for(var y_pos = 0; y_pos < col; y_pos++){
             var nNeighbors = countNeighbors(x_pos, y_pos);
-            daRules(x_pos,y_pos,nNeighbors, copyGrid);
+            daRules(x_pos, y_pos, nNeighbors, copyGrid);
         }
     }
-    return copyGrid;
-}
-
-
-function makeAlive(xSpot, ySpot){
-    if(xSpot <= 0 || ySpot <= 0){
-		return;
+    
+    //Set cell dead or alive for the next generation
+    for (var i = 0; i < row; i++) {
+		for (var j = 0; j < col; j++) {
+			grid[i][j] = copyGrid[i][j];
+			if (grid[i][j] == 1) {
+				document.getElementById(i + '_' + j).setAttribute('class', 'live');
+			} else {
+				document.getElementById(i + '_' + j).setAttribute('class', 'dead');
+			}
+		}
 	}
-    if(grid[xSpot][ySpot] == 1) { //dead
-		grid[xSpot][ySpot] = 0;
-	} else if (grid[xSpot][ySpot] == 0) { // alive
-		grid[xSpot][ySpot] = 1;
-	}
 }
 
-//get mouse position on click
-//Reference W3 Schools
-function getMousePosition(event) {
-    let rect = canvas.getBoundingClientRect();
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-    console.log("Coordinate x: " + x,"Coordinate y: " + y);
-    makeAlive(x,y);
-}
-
-// mouse click listener
-let canvasElem = document.querySelector('canvas');
-canvasElem.addEventListener("mousedown", function(e){getMousePosition(e);});
 
 
 //rule implementations for new generation
@@ -111,46 +191,68 @@ canvasElem.addEventListener("mousedown", function(e){getMousePosition(e);});
 // Any live cell with more than three live neighbors will DIE
 // Any live cell with two or three live neighbors live onto next generation
 // Any dead cell with exactly three live neighbors becomes a live cell
-function daRules(column, rows, nNeighbors, copyGrid){
-    var copyGrid = grid.map(elem => elem);
+function daRules(column, row, nNeighbors, copyGrid){
+    
     //rules
-    if(grid[column][rows] == 1 && nNeighbors < 2) {
+    if(grid[column][row] == 1 && nNeighbors < 2) {
         //If current cell is alive and numNeighbors is less than 2
         //Current cell will die in the next generation due to underpopulation
-        copyGrid[column][rows] = 0;
-    } else if(grid[column][rows] == 1 && nNeighbors > 3){
+        copyGrid[column][row] = 0;
+    } else if(grid[column][row] == 1 && nNeighbors > 3){
         //If current cell is alive and numNeighbors is more than 2
         //Current cell will die in the necolumnt generation due to overpopulation
-        copyGrid[column][rows] = 0;
-    } else if(grid[column][rows] == 0 && nNeighbors == 3){
+        copyGrid[column][row] = 0;
+    } else if(grid[column][row] == 0 && nNeighbors == 3){
         //If current cell is dead and numNeighbors equals to 3
         //Current cell will become alive in the next generation
-        copyGrid[column][rows] = 1;
+        copyGrid[column][row] = 1;
     }
     return copyGrid;
+}
+
+function fillCell(aGrid, row, col){
+    if(row <= 0 || col <= 0){
+        return;
+    }
+    if(aGrid[row][col] == 1){
+        var cell = document.getElementById(row + '_' + col).setAttribute('class', 'dead');
+		aGrid[row][col] = 0;
+	} else if (aGrid[row][col] == 0) {
+		var cell = document.getElementById(row + '_' + col).setAttribute('class', 'live');
+		aGrid[row][col] = 1;
+	}
+}
+
+//clear the grid
+function clearGrid(){
+    disableBoard = false;
+    cancelAnimationFrame(stopID);
+    reset();
 }
 
 // Credit: https://www.youtube.com/watch?v=tS6oP1NveoI
 //stopper
 function stopGame(){
-    enableBoard = false;
+    disableBoard = false;
     cancelAnimationFrame(stopID);
 }
 
 // starter
 function startGame(){
-    if(enableBoard){
+    if(disableBoard){
         return;
     }
+    disableBoard = true;
+    displayNextGen();
     window.requestAnimationFrame(update);
-    enableBoard = true;
 }
 //End Credit---
 
 //looper
 function update(){
-    grid = displayNextGen();
-    render();
+    disableBoard = true;
+    displayNextGen();
+    // render();
     stopID = requestAnimationFrame(update);
 }
 
