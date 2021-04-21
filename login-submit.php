96cfdcfc4e89b1460d7c03737e0073504702b537
kyoung224 <60451182+kyoung224@users.noreply.github.com>
2021-04-20 20:08:59 -0400

    Add files via upload

--- login-submit.php
+++ login-submit.php
@@ -9,7 +9,7 @@
       $username = $_POST['username'];
       $password = $_POST['password'];
       $file1 = file("users.txt");
-      $user_input = '';
+      $user_saved = '';
 
       if ($username == ''){
         echo "<script>
@@ -23,34 +23,29 @@
               </script>";
       } else{
         for ($i = 0; $i < count($file1); $i++){
-          $user_input = strstr($file1[$i], $username);
-          if($user_input !== false) {
+          $user_saved = strstr($file1[$i], $username);
+          if($user_saved !== false) {
             break;
           }
         }
-        if ($user_input == ''){
+        if ($user_saved == ''){
           echo "<script>
                 alert('That Username does not exist. Please try again');
                 window.location.replace('./login.php');
                 </script>";
         } else{
-          $user_inputArr = explode(",", $user_input);
-          $input_password = $user_inputArr[1];
-
-          if (strcmp($password, $input_password) !== 0){
-            echo strcmp($password, $input_password);
-            echo $password;
-            echo "<br>";
-            echo $input_password;
-            echo "<br>";
-            echo $username;
-            echo "<br>";
-            echo $user_input;
-
+          $user_savedArr = explode(",", $user_saved);
+          $saved_password = $user_savedArr[1];
+          
+          if ($password != $saved_password){
+            echo "<script>
+                  alert('That Password does not match with the Username. Please try again');
+                  window.location.replace('./gamepage.html');
+                  </script>";
           } else{
             echo "<script>
                   alert('Welcome back! Redirecting to gamepage now');
-                  window.location.replace('./conwayViaCanvas.html');
+                  window.location.replace('./gamepage.html');
                   </script>";
           }
         }
